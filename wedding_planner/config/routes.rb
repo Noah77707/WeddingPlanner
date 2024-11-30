9b07c85356f65191f0cb98e67b42c076016b7d09
Noah77707 <Noah.hoepfinger@gmail.com>
2024-11-30 15:06:15 -0700

    Got calendar and adding events working. Yippee

--- wedding_planner/config/routes.rb
+++ wedding_planner/config/routes.rb
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   get "/redirect", to: "calendars#redirect"
   get "/callback", to: "calendars#callback"
   get "/calendars", to: "calendars#calendars"
-  get '/events/:calendar_id', to: 'example#events', as: 'events', calendar_id: /[^\/]+/
-  post '/events/:calendar_id', to: 'example#new_event', as: 'new_event', calendar_id: /[^\/]+/
+  get '/events/:calendar_id', to: 'calendars#events', as: 'events', calendar_id: /[^\/]+/
+  post '/events/:calendar_id', to: 'calendars#new_event', as: 'new_event', calendar_id: /[^\/]+/
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
